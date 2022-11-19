1983ba1726899ad119c5e51c70e0b998475c2488
MarcosGaray <102810449+MarcosGaray@users.noreply.github.com>
2022-11-19 17:53:51 -0300

    asdf

--- Alojamiento.cs
+++ Alojamiento.cs
@@ -16,7 +16,7 @@ namespace TP_II
         protected List<Reserva> reservas = new List<Reserva>();
         protected Image[] imagenes;
 
-
+        //prueba
         public Alojamiento(string direccion)
         {
             this.direccion = direccion;
